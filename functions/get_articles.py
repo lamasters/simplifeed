574e8b968a0fd5070d35e0100461b26a4853e3df
lamasters <liam.masters3@gmail.com>
2023-10-14 17:37:24 -0400

    Test

--- functions/get_articles.py
+++ functions/get_articles.py
@@ -171,4 +171,5 @@ async def main(context):
 
     json_data = [jsonable_encoder(res) for res in res_data]
     context.log(f"Returning data {json_data}")'''
-    return context.res.json({"status": http.HTTPStatus.OK, "data": "test"})
+    data=[jsonable_encoder(ArticleContentRes(data=ArticleContent(tags=["test"])))]
+    return context.res.json({"status": http.HTTPStatus.OK, "data": data})
