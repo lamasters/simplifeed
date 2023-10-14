52ccac95291af8f84f095413b8e13d3531158ded
lamasters <liam.masters3@gmail.com>
2023-10-14 16:57:54 -0400

    Update to test

--- functions/get_articles.py
+++ functions/get_articles.py
@@ -167,8 +167,8 @@ async def main(context):
 
     if not res_data:
         context.log("No data fetched")
-        return context.res.json({"status": http.HTTPStatus.BAD_REQUEST, "data": None})
+        return context.res.json({"status": http.HTTPStatus.BAD_REQUEST, "data": "test"})
 
     json_data = [jsonable_encoder(res) for res in res_data]
     context.log(f"Returning data {json_data}")
-    return context.res.json({"status": http.HTTPStatus.OK, "data": json_data})
+    return context.res.json({"status": http.HTTPStatus.OK, "data": "test"})
