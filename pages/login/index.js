a651be66241cf92a5a974f3be25631455a6c06f2
Liam Masters <liam.masters3@gmail.com>
2024-02-05 16:37:13 -0500

    Update redirect URLs

--- pages/login/index.js
+++ pages/login/index.js
@@ -52,8 +52,8 @@ export default function Login() {
                             try {
                                 session.account.createOAuth2Session(
                                     'google',
-                                    'https://simplifeed.org',
-                                    'https://simplifeed.org/login'
+                                    window.location.origin,
+                                    `${window.location.origin}/login`
                                 );
                             } catch (e) {
                                 console.error(e);
