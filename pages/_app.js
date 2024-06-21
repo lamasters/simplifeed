012926089e36b53d31bbc12cabe2efbec1cebd45
lamasters <liam.masters3@gmail.com>
2024-06-21 12:38:46 -0400

    Fix formatting

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
