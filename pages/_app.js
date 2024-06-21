e50cf842eeb1568be596af2b24210403d81910ae
lamasters <liam.masters3@gmail.com>
2024-06-21 12:19:23 -0400

    Add vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
         <>
             <Component {...pageProps} />
             <Analytics />
+            <SpeedInsights />
         </>
     );
 }
