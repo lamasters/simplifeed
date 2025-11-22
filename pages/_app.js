fe2e3de48a757b46562a3aa14ba3ee1795317cf1
lamasters <liam@liammasters.space>
2025-11-21 20:16:22 -0500

    Implement global audio player

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,17 @@ import '../styles/player-style.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { PlayerProvider } from '../components/player-context';
+import GlobalPlayer from '../components/global-player';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
     return (
-        <>
+        <PlayerProvider>
             <Component {...pageProps} />
+            <GlobalPlayer />
             <Analytics />
             <SpeedInsights />
-        </>
+        </PlayerProvider>
     );
 }
