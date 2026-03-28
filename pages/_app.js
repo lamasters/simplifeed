ddcb23c5365a11b684ab20023752c63d970f106d
lamasters <liam.masters3@gmail.com>
2026-03-27 20:26:25 -0400

    Fix formatting

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import '../styles/globals.css';
 import '../styles/player-style.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { PlayerProvider } from '../components/player-context';
 import GlobalPlayer from '../components/global-player';
+import Head from 'next/head';
+import { PlayerProvider } from '../components/player-context';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
